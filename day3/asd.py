51eebb9cc1a854b78661703c877bb2c4ccaf34bc
Sami Harju <sami.harju@gmail.com>
2022-12-04 14:45:05 +0200

    day3

---
+++ day3/asd.py
@@ -0,0 +1,26 @@
+from string import ascii_letters
+
+
+def solve(data):
+    sacks = [
+        (set(s[: int(len(s) / 2)]), set(s[int(len(s) / 2) :])) for s in data.split("\n")
+    ]
+    return sum([ascii_letters.index(d) + 1 for s in sacks for d in s[0] & s[1]])
+
+
+def solve2(data):
+    elfss = data.split("\n")
+    return sum(
+        [
+            ascii_letters.index(d) + 1
+            for g in [elfss[i : i + 3] for i in range(0, len(elfss), 3)]
+            for d in set(g[0]) & set(g[1]) & set(g[2])
+        ]
+    )
+
+
+if __name__ == "__main__":
+    with open("input1.txt") as f:
+        d = f.read()
+        print(solve(d))
+        print(solve2(d))
---
+++ day3/test.py
@@ -0,0 +1,24 @@
+from asd import solve, solve2
+
+
+def test_part1():
+
+    data = """vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw"""
+    assert solve(data) == 157
+
+
+def test_part2():
+
+    data = """vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw"""
+
+    assert solve2(data) == 70
